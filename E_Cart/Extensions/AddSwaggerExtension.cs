2fdf00baeab860cb7cb415449c76e5e6ad0b0a39
queens kisivuli <queenskisivuli@gmail.com>
2023-09-05 23:31:14 +0300

    swagger auth config

---
+++ E_Cart/Extensions/AddSwaggerExtension.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+namespace E_Cart.Extensions
+{
+    public static class AddSwaggerExtension
+    {
+        public static WebApplicationBuilder AddSwaggenGenExtension(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Provide a Valid Token",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference= new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id=JwtBearerDefaults.AuthenticationScheme
+                            }
+                        }, new string[]{}
+
+                    }
+                });
+            });
+            return builder;
+        }
+    }
+}
