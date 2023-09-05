913e54d065ff2855c9090c99226b93f32eaa3dcf
queens kisivuli <queenskisivuli@gmail.com>
2023-09-05 23:44:07 +0300

    auth profile

---
+++ E_Auth/Profiles/AuthProfiles.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using E_Auth.Model;
+using E_Auth.Model.Dtos;
+
+namespace E_Auth.Profiles
+{
+    public class AuthProfiles: Profile
+    {
+        public AuthProfiles()
+        {
+            CreateMap<RegisterRequestDto, ApplicationUser>()
+                .ForMember(u => u.UserName, opt => opt.MapFrom(x => x.Email));
+            CreateMap<ApplicationUser, UserDto>();
+        }     
+    }
+}
